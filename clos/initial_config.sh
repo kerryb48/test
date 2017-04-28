41df9abdb871a288397a8f185f663add6354a3fa
Pavel May <pavelmay@snaproute.com>
2017-04-28 13:14:37 -0400

    Explicit statements

--- clos/initial_config.sh
+++ clos/initial_config.sh
@@ -9,7 +9,7 @@
 IFS=","
 echo "BGPGlobal settings"
 cat containers.lst | while read cid name ns ip; do
-  echo "fluffy.py --host $ip BGPGlobal config PATCH --json_blob '{\"ASNum\":\"$ns\",\"RouterID\":$ns}'"
+  echo "fluffy.py --host $ip BGPGlobal config PATCH --json_blob '{\"ASNum\":\"$ns\",\"RouterId\":$ns, \"Disabled\":\"false\"}'"
 done
 
 echo "LLDPGlobal settings"
