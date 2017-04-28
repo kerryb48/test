2d253284c68f99208eb71d59c8cc86a9f5f945ac
Pavel May <pavelmay@snaproute.com>
2017-04-27 21:28:52 -0400

    RouterId is an INT, not a quoted string

--- clos/initial_config.sh
+++ clos/initial_config.sh
@@ -9,7 +9,7 @@
 IFS=","
 echo "BGPGlobal settings"
 cat containers.lst | while read cid name ns ip; do
-  echo "fluffy.py --host $ip BGPGlobal config PATCH --json_blob '{\"ASNum\":\"$ns\",\"RouterID\":\"$name:$ns\"}'"
+  echo "fluffy.py --host $ip BGPGlobal config PATCH --json_blob '{\"ASNum\":\"$ns\",\"RouterID\":$ns}'"
 done
 
 echo "LLDPGlobal settings"
