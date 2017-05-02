0e817cabf4b646ba72792963a143a8a81efba934
Pavel May <pavelmay@snaproute.com>
2017-05-02 10:35:35 -0400

    Better messages

--- clos/docker_check_netlinks.sh
+++ clos/docker_check_netlinks.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -o pipefail
 
+if [ $# -ne 1 ]; then
+  echo "Expected one parameters - filename containing the netlinks created by the companion script"
+  exit 1
+fi # if [ $# -ne 1 ]
+
 cat $1 | sed 's/,/ /g' | while read srcinst srcnic dstinst dstnic; do
   echo -n "Checking link from $srcinst:$srcnic to $dstinst:$dstnic: "
   $(~/code/docker_list_veths.sh $srcinst $dstinst|grep "^$srcinst $srcnic"|grep --quiet "$dstinst $dstnic")
@@ -8,6 +13,6 @@ cat $1 | sed 's/,/ /g' | while read srcinst srcnic dstinst dstnic; do
     echo "Link OK"
   else
     echo "Link NOT OK" 
-    echo "Try running docker_fix_netlink.sh $srcinst $srcnic $dstinst $dstnic to fix"
+    echo "Try running \"docker_fix_netlink.sh $srcinst $srcnic $dstinst $dstnic\" to fix"
   fi
 done
